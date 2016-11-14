8d18800d88204e80cbdb868d007a8ee87751c291
Mesut Öztürk <mesutoztrk@msn.com>
2016-11-14 10:00:03 +0300

    added EntityFramework CodeFirst DbContext

---
+++ ASPNetWebApi/ASPNetWebApi/Migrations/201611140658394_firstmigrationfromdb.cs
@@ -0,0 +1,16 @@
+namespace ASPNetWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigrationfromdb : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
